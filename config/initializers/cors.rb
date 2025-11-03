ea22e701a4764705ad56a463002eccf54b39a7df
Dean Lofts <dean@deanlofts.xyz>
2025-11-03 12:52:30 +1100

    allow vercel

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,7 +4,11 @@
 if defined?(Rack::Cors)
   Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins "localhost:3001", "127.0.0.1:3001", "http://localhost:3001", "http://127.0.0.1:3001"
+      origins "localhost:3001",
+              "127.0.0.1:3001",
+              "http://localhost:3001",
+              "http://127.0.0.1:3001",
+              "https://techub-battles.vercel.app"
 
       resource "/api/*",
         headers: :any,
